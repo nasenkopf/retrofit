e6ca9d30dfdc823801e7d9bb729af6d1e6fa38a7
Jörg <@>
2017-06-02 12:59:21 +0200

    example

---
+++ app/src/main/java/com/example/john_smith/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.john_smith.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/example/john_smith/myapplication/WorkoutDetailFragment.java
@@ -0,0 +1,9 @@
+package com.example.john_smith.myapplication;
+
+/**
+ * Created by john_smith on 09.05.17.
+ */
+
+public class WorkoutDetailFragment {
+
+}
