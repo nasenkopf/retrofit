85179f0ac4da5e0a20f5a99d2091547411fc58d3
nasenkopf <lay-z@gmx.de>
2017-06-26 17:11:01 +0200

    Update ApiInterace.java

--- app/src/main/java/liese/jrg/gitorg/ApiInterace.java
+++ app/src/main/java/liese/jrg/gitorg/ApiInterace.java
@@ -4,19 +4,14 @@ package liese.jrg.gitorg;
  * Created by john_smith on 19.06.17.
  */
 
-/**
- * Created by john_smith on 19.06.17.
- */
-
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 
-/**
- * Created by Jörg.Liese on 16.06.2017.
- */
-
+//Schnittstelle die sich mit einem Befehl die Organisationen holt
+//Erstellt eine Liste von Objekten der Klasse Organisation
+//Speichert in der Methode orgList(); ab 
 public interface ApiInterace {
     @GET("/organizations")
     Call<List<Organization>> orgList();
