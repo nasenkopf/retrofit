30cc26af14bd6d1a2ff6b6b3ced1372c0ffebf5b
nasenkopf <lay-z@gmx.de>
2017-06-26 17:11:35 +0200

    Update MainActivity.java

--- app/src/main/java/liese/jrg/gitorg/MainActivity.java
+++ app/src/main/java/liese/jrg/gitorg/MainActivity.java
@@ -1,5 +1,6 @@
 package liese.jrg.gitorg;
 
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -30,26 +31,32 @@ public class MainActivity extends AppCompatActivity {
 
         where = new ArrayList<String>();
 
+		//API Verbindung aufgerufen
         Retrofit.Builder builder = new Retrofit.Builder()
                 .baseUrl("https://api.github.com/")
                 .addConverterFactory(GsonConverterFactory.create());
-
+		//Aufruf Verbindung zu Retrofit
         Retrofit retrofit = builder.build();
-
+		//Aufruf Klasse API 
         ApiInterace client = retrofit.create(ApiInterace.class);
         Call<List<Organization>> call = (Call<List<Organization>>) client.orgList();
 
+		//Queue wird erstellt
         call.enqueue(new Callback<List<Organization>>() {
             @Override
             public void onResponse(Call<List<Organization>> call, Response<List<Organization>> response) {
-                List<Organization> repos = response.body();
+                List<Organization> repos = response.body();//Zeigt immer 30 Objekte an
                 for (int i = 0; i < 30; i++) {
-                    Log.d("Name", response.body().get(i).getLogin());
+                    Log.d("Name", response.body().get(i).getLogin());//Ausgabe für den Entwickler, User sieht es nicht
                     if (repos.get(i).getDescription() != null && !repos.get(i).getDescription().toString().equals("")) {
-                        Log.d("Beschreibung", response.body().get(i).getDescription());
-                        where.add(repos.get(i).getLogin()+": \n"+repos.get(i).getDescription());
+						
+						
+                        Log.d("Beschreibung", response.body().get(i).getDescription()); //gibt nur Beschreibung aus wenn diese worhanden ist
+						
+	
+                        where.add(repos.get(i).getLogin()+": \n"+repos.get(i).getDescription()); //where ist ein Array und wird der listviewhinzugefügt, wird dem User angezeigt
                     }else{
-                        where.add(repos.get(i).getLogin());
+                        where.add(repos.get(i).getLogin()); //sonst nur den Login
                     }
                 }
                 adapter = new ArrayAdapter<String>(getApplication(), android.R.layout.simple_list_item_1, where);
